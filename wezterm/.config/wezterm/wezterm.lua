352d4a32659a87dcb42c1fe92e58976a9dd9a496
darko-mesaros <darko.subotica@gmail.com>
2024-07-04 22:23:40 -0700

    Made it work with my Lenovo P14

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,7 +7,8 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Campbell (Gogh)'
+--config.color_scheme = 'Campbell (Gogh)'
+config.color_scheme = 'Dark Pastel (Gogh)'
 
 -- FONTS
 config.font = wezterm.font 'Iosevka Nerd Font Mono'
