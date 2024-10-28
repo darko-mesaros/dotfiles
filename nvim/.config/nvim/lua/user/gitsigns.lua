8643a34f38756ea7d3deae4887f369e218524d3c
darko-mesaros <darko.subotica@gmail.com>
2024-10-27 20:02:57 -0700

    some fixes and mason stuff

--- nvim/.config/nvim/lua/user/gitsigns.lua
+++ nvim/.config/nvim/lua/user/gitsigns.lua
@@ -5,11 +5,20 @@ end
 
 gitsigns.setup {
   signs = {
-    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "󰐊", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "󰐊", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    add = { text = "▎"},
+    change = { text = "▎"},
+    delete = { text = "󰐊"},
+    topdelete = { text = "󰐊"},
+    changedelete = { text = "~"},
+    untracked    = { text = '┆' },
+  },
+  signs_staged = {
+    add = { text = "▎"},
+    change = { text = "▎"},
+    delete = { text = "󰐊"},
+    topdelete = { text = "󰐊"},
+    changedelete = { text = "~"},
+    untracked    = { text = '┆' },
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
